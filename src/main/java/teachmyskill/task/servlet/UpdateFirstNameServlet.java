97c755ece7020f31368b584302c0f5d4c03c2c7c
Aastapchik <154362326+Aastapchik@users.noreply.github.com>
2024-06-13 16:11:05 +0300

    Update UpdateFirstNameServlet.java

--- src/main/java/teachmyskill/task/servlet/UpdateFirstNameServlet.java
+++ src/main/java/teachmyskill/task/servlet/UpdateFirstNameServlet.java
@@ -12,25 +12,28 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import static teachmyskill.task.database.Connect.getConnect;
+
 @WebServlet("/change-login")
 public class UpdateFirstNameServlet extends HttpServlet {
 
-
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            int id = Integer.parseInt(req.getParameter("id"));
-            String newFistName = req.getParameter("first_name");
-            Connection connection = getConnect();
-            try {
-                PreparedStatement preparedStatement = connection.prepareStatement("UPDATE EMPLOYEES SET FIRST_NAME = ? WHERE EMPLOYEE_ID = ?");
-                preparedStatement.setString(1, newFistName);
-                preparedStatement.setInt(2, id);
-                preparedStatement.executeUpdate();
-                resp.getWriter().write("SUCCESSFUL UPDATE FIRST NAME!!");
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        String newFistName = req.getParameter("first_name");
+        Connection connection = null;
 
+        try {
+            connection = getConnect();
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE EMPLOYEES SET FIRST_NAME = ? WHERE EMPLOYEE_ID = ?");
+            preparedStatement.setString(1, newFistName);
+            preparedStatement.setInt(2, id);
+            preparedStatement.executeUpdate();
+            resp.getWriter().write("SUCCESSFUL UPDATE FIRST NAME!!");
+            getServletContext().getRequestDispatcher("/WEB-INF/updateUser.jps").forward(req, resp);
+        } catch (Exception e) {
+            getServletContext().getRequestDispatcher("/WEB-INF/errorPage.jsp").forward(req, resp);
         }
+
     }
+}
 
