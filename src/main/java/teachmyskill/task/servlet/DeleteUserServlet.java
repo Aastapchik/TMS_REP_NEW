cb5158a21e97242151f303ad4a469e71b8faab2f
Aastapchik <154362326+Aastapchik@users.noreply.github.com>
2024-06-13 16:10:20 +0300

    Update DeleteUserServlet.java

--- src/main/java/teachmyskill/task/servlet/DeleteUserServlet.java
+++ src/main/java/teachmyskill/task/servlet/DeleteUserServlet.java
@@ -9,24 +9,28 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import static teachmyskill.task.database.Connect.getConnect;
 @WebServlet("/delete")
 public class DeleteUserServlet extends HttpServlet {
 
+
+
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int id = Integer.parseInt(req.getParameter("id"));
-        Connection connection = getConnect();
+        Connection connection = null;
         try {
+            connection = getConnect();
             PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM EMPLOYEES WHERE EMPLOYEE_ID = ?");
             preparedStatement.setInt(1, id);
             preparedStatement.executeUpdate();
             resp.getWriter().write("SUCCESSFUL DELETE!");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            req.setAttribute("name", id);
+            getServletContext().getRequestDispatcher("/WEB-INF/deleteUser.jsp").forward(req,resp);
+        } catch (Exception e) {
+            getServletContext().getRequestDispatcher("/WEB-INF/errorPage.jsp").forward(req, resp);
         }
 
     }
