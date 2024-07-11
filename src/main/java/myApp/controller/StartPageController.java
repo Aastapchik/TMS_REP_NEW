c1b410695bc71c33abe9d1820ae7aaa4f95abce4
Aastapchik <aastapchik@inbox.ru>
2024-07-11 11:29:18 +0300

    Spring BOOT 43 LESSON
    (cherry picked from commit 26a60d4bf4425efe0fbe1e33684b2eff8c50b1a1)

---
+++ src/main/java/myApp/controller/StartPageController.java
@@ -0,0 +1,12 @@
+package myApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class StartPageController {
+
+    @GetMapping("/")
+    public String startingPage(){
+        return "startingPage";
+    }
+}
